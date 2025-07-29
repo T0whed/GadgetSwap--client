5cca26225446f993015712422aeba329d90ae880
Md. Saminul Amin <saminul.amin@gmail.com>
2025-07-29 17:41:05 +0600

    issues fixed

---
+++ src/Pages/DashboardPage/AdminDashboardPages/AdminAllMessagesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminAllMessagesComponent from "../../../Components/DashboardComponent/AdminComponents/AdminAllMessagesComponent.jsx";
+
+const AdminAllMessagesPage = () => {
+  return (
+    <div>
+      <AdminAllMessagesComponent></AdminAllMessagesComponent>
+    </div>
+  );
+};
+
+export default AdminAllMessagesPage;
---
+++ src/Pages/DashboardPage/AdminDashboardPages/AdminAllRentalsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminAllRentalsComponent from "../../../Components/DashboardComponent/AdminComponents/AdminAllRentalsComponent.jsx";
+
+const AdminAllRentalsPage = () => {
+  return (
+    <div>
+      <AdminAllRentalsComponent></AdminAllRentalsComponent>
+    </div>
+  );
+};
+
+export default AdminAllRentalsPage;
